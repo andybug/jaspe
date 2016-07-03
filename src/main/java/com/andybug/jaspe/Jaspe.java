5dd2fde4d7765c8f91fa1a0fd02d9deeeda47527
Andrew Fields <andybug10@gmail.com>
2016-07-03 16:41:59 -0500

    Read arguments and call ConfigParser

--- src/main/java/com/andybug/jaspe/Jaspe.java
+++ src/main/java/com/andybug/jaspe/Jaspe.java
@@ -1,13 +1,25 @@
 package com.andybug.jaspe;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileNotFoundException;
+
+
 public class Jaspe
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("jaspe!");
+
+	if (args.length < 1) {
+	    System.err.println("jaspe <config.yaml>");
+	    System.exit(1);
+	}
+
+	try {
+	    ConfigParser configParser = new ConfigParser(args[0]);
+	    Config config = configParser.parse();
+	} catch (FileNotFoundException fnfe) {
+	    System.err.println("Could not open '" + fnfe.getMessage() + "'");
+	    System.exit(1);
+	}
     }
 }
