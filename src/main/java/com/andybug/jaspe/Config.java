69f10e39e7265b21d47b903b67820365c05c36f0
Andrew Fields <andybug10@gmail.com>
2016-07-04 09:24:19 -0500

    Implement simple Config class

--- src/main/java/com/andybug/jaspe/Config.java
+++ src/main/java/com/andybug/jaspe/Config.java
@@ -2,4 +2,47 @@ package com.andybug.jaspe;
 
 class Config
 {
+
+    /* internal classes */
+
+    static class ServerPorts
+    {
+	public short jaspe;
+	public short redis;
+	public short postgres;
+    }
+
+
+    /* document members */
+
+    public ServerPorts servers;
+
+
+    /* methods */
+
+    public boolean validate()
+    {
+	boolean retval = true;
+
+	/* validate ports */
+	retval = retval && validatePort(servers.jaspe);
+	retval = retval && validatePort(servers.redis);
+	retval = retval && validatePort(servers.postgres);
+
+	return retval;
+    }
+
+
+    /* private methods */
+
+    private boolean validatePort(short port)
+    {
+	final int PORT_MAX = (1 << 16) - 1;
+
+	if (port < 0 || port > PORT_MAX)
+	    return false;
+
+	return true;
+    }
+
 }
